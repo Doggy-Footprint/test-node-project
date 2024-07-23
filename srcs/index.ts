c52a69178e6ead4fefe96d4115b4e7fae4fbbcfd
Concepty <khs1903b@gmail.com>
2024-07-23 10:58:52 +0900

    test: initialization of object

--- srcs/index.ts
+++ srcs/index.ts
@@ -2,4 +2,34 @@ export * from './test'
 
 console.log('running');
 
+
+
+
+
+if (true) {
+    class A {
+        value: number = 0;
+        date: Date = new Date();
+        date2: Date;
+    
+        constructor(value?: number) {
+            if (value) this.value = value;
+            this.date2 = new Date();
+        }
+    }
+    console.log('---test initialization---');
+
+    const a = new A();
+    const b = new A(123);
+
+    console.log(`a.date: ${a.date.getTime()}, a.date2: ${a.date2.getTime()}`);
+
+    console.log(`b.date: ${b.date.getTime()}, b.date2: ${b.date2.getTime()}`);
+    // can't get order between date and date2, but it seems same in millisecond level
+}
+
+
+
+
+
 console.log('ended');
